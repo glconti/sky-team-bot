f907c6b19ef4cdaec9ebd72d53fa4bf19d04f773
Gianluigi Conti <13535297+glconti@users.noreply.github.com>
2025-10-20 23:28:55 +0200

    Add altitude tests

---
+++ SkyTeam.Domain.Tests/AltitudeTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace SkyTeam.Domain.Tests;
+
+public class AltitudeTests
+{
+    [Fact]
+    public void ctor_ShouldInitializeAltitudeCorrectly()
+    {
+        // Arrange & Act
+        var altitude = new Altitude();
+
+        // Assert
+        altitude.CurrentAltitude.HasReroll.Should().BeTrue();
+        altitude.CurrentPlayer.Should().Be(Player.Pilot);
+    }
+
+    [Fact]
+    public void Advance_UpdatesAltitudeAndPlayerTurn()
+    {
+        // Arrange
+        var altitude = new Altitude();
+
+        // Act
+        altitude.Advance();
+
+        // Assert
+        altitude.CurrentAltitude.HasReroll.Should().BeFalse();
+        altitude.CurrentPlayer.Should().Be(Player.Copilot);
+    }
+}
--- SkyTeam.Domain/Altitude.cs
+++ SkyTeam.Domain/Altitude.cs
@@ -2,22 +2,36 @@
 
 class Altitude
 {
-    private readonly Queue<AltituteSegment> _altituteSegments = new(
+    private readonly Queue<AltitudeSegment> _altitudeSegments = new(
     [
-        new(), // 6000
+        new(true), // 6000
         new(), // 5000
         new(), // 4000
         new(), // 3000
-        new(), // 2000
+        new(true), // 2000
         new(), // 1000
         new() // 0 Landed
     ]);
 
-    public int CurrentAltitude => _altituteSegments.Count;
+    public AltitudeSegment CurrentAltitude => _altitudeSegments.Peek();
 
-    public void Advance() => _altituteSegments.Dequeue();
+    public Player CurrentPlayer { get; private set; } = Player.Pilot;
+
+    public void Advance()
+    {
+        _altitudeSegments.Dequeue();
+
+        CurrentPlayer = CurrentPlayer == Player.Pilot ? Player.Copilot : Player.Pilot;
+    }
 }
 
-class AltituteSegment
+class AltitudeSegment(bool hasReroll = false)
 {
+    public bool HasReroll { get; } = hasReroll;
+}
+
+enum Player
+{
+    Pilot,
+    Copilot
 }
